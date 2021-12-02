4888528b4a9c0b6ebb83e53d16eb49d47995d166
anakamahesh <71583665+anakamahesh@users.noreply.github.com>
2021-12-01 16:36:44 -0800

    Update laptop_pub.py

--- laptop_pub.py
+++ laptop_pub.py
@@ -1,12 +1,13 @@
-# Laptop pub
 """EE 250 Final Project
 Team: Chiara Di Camillo, Anaka Mahesh
-Run laptop_pub.py in a separate terminal on your VM."""
+Run laptop_pub.py in a separate terminal on VM."""
 
 import paho.mqtt.client as mqtt
 import time
 from pynput import keyboard
 import sys
+
+IS_ON = False
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -17,23 +18,34 @@ def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 def on_press(key):
+    global IS_ON
     try: 
         k = key.char # single-char keys
     except: 
         k = key.name # other keys
     
-    if k == '0':
-        print("0")
-        client.publish("cdam/lcd","Device Off")            # Send message to rpi
-    elif k == '1':
-        print("1")
-        client.publish("cdam/lcd","Device On")             # Send message to rpi
-    elif k == 'f':
-        print("f")
-        client.publish("cdam/lcd", "Listening female")   # Send "f" character to rpi
-    elif k == 'm':
-        print("m")
-        client.publish("cdam/lcd", "Listening male")     # Send "m" character to rpi
+    if k == 'a':
+        if IS_ON == False:
+            IS_ON = True
+            print("\nON")
+            client.publish("cdam/lcd","ON")            # Send message to rpi
+        else:
+            return
+    elif k == 'b':
+        if IS_ON == True:
+            IS_ON = False
+            print("\nOFF")
+            client.publish("cdam/lcd","OFF")             # Send message to rpi
+        else:
+            return
+
+    if IS_ON == True:
+        if (k == 'f'):
+            print("\nListening for female voice")
+            client.publish("cdam/lcd", "f")   # Send "f" character to rpi
+        elif (k == 'm'):
+            print("\nListening for male voice")
+            client.publish("cdam/lcd", "m")     # Send "m" character to rpi
 
 if __name__ == '__main__':
     #setup the keyboard event listener
